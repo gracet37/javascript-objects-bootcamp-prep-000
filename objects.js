737203c3fb4e8f07873d64309cc5c5a2b1ca9e38
Grace Tan <gracetan37@gmail.com>
2019-02-06 06:21:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artist: "boogywoogy"}
 
-function updatePlaylist(obj, key, value) {
-  return Object.assign({}, obj, {artist: ["My Bloody Valentine"]})
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {artistName: "songTitle"})
 }
