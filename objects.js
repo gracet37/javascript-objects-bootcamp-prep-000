ff205f12368a2a9b1211522cd7a97f239a6d3d32
Grace Tan <gracetan37@gmail.com>
2019-02-06 06:37:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,5 @@
 var playlist = {}
 var playlist = {"Britney Spears": "Hit Me Baby"}
+function updatePlaylist(playlist,artistName,songTitle) {
+  return Object.assign({},playlist, {Slowdrive: "Alison", "My Bloody Valentine": "Sometimes", "Phil Oaks": "Here's to the states of Mississippi"})
+}
